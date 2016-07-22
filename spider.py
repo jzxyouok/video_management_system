285a9f9127a469f38a913abe3c13c2370b0b67e8
oliverfan <472161613@qq.com>
2016-07-22 13:50:11 +0800

    添加了视频资源的序列化（json）； 添加了存入txt文件功能； 添加了程序运行计时功能； ps：multiConnect()可能会产生问题，暂时没有使用这个函数。

--- spider.py
+++ spider.py
@@ -1,6 +1,9 @@
 #coding=utf-8
 import urllib2
 from lxml import etree
+import time
+import math
+import json
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -14,7 +17,7 @@ def multiConnect(request):
         print "网络异常，正在重新尝试连接..."
         multiConnect(request)
 
-
+start_time = time.clock()
 
 #爬取视频广告的分类
 url = "http://k.cnad.com"
@@ -34,9 +37,10 @@ for category_item in category_list:
     category_item_dict.append(category_item_dictitem)
 print category_item_dict
 
-
+production_box_item_list_total = []
 for category_item in category_item_dict:
     # 爬取某一类别的视频广告的页面数量
+    production_box_item_list = []
     page_index = 1
     url = category_item['href'] + "&page=" + str(page_index)
     request = urllib2.Request(url)
@@ -49,11 +53,15 @@ for category_item in category_item_dict:
     print "当前分类视频资源页面数共计：" + page_count + "页"
 
     # 爬取视频广告的具体信息
-    page_index = 1
     for page_index in range(int(page_count)):
         url = category_item['href'] + "&page=" + str(page_index + 1)
         request = urllib2.Request(url)
-        response = multiConnect(request)
+        # response = multiConnect(request)
+        try:
+            response = urllib2.urlopen(request)
+        except:
+            print "网络异常，正在重新尝试连接..."
+            response = urllib2.urlopen(request)
         html = response.read()
         tree = etree.HTML(html, parser=etree.HTMLParser(encoding='utf-8'))
         # 注释掉的这两行xpath解析的是k.cnad.com这个路径下的页面，它存在分类栏这个结构
@@ -62,19 +70,48 @@ for category_item in category_item_dict:
         production_box_list = tree.xpath(u'//div[@class="main_box"]/div[@class="production_box"]')
         production_index = 1
         for production_box in production_box_list:
-            print "*****************当前是第", page_index * 24 + production_index, "个视频资源，信息如下*****************"
-            print "视频名称", production_box[1][0].text.lstrip()
-            print "品牌名称：", production_box[2][0].text
-            print "语种：", production_box[3][0].text
-            print "国家或地区：", production_box[3][1].text
-            print "广告年份：", production_box[4][0].text
-            print "广告类型：", production_box[4][1].text
-            video_cover_image_link = production_box[0][0][0].get('src')
-            if video_cover_image_link[0:4] == "http":
-                print "广告视频封面图片链接:", production_box[0][0][0].get('src')
-            else:
-                print "广告视频封面图片链接:", "http://k.cnad.com" + production_box[0][0][0].get('src')
-            print "广告视频资源链接：", "http://k.cnad.com" + production_box[0][0].get('href')
-            print "**************************分割线***************************************"
-
+            # print "*****************当前是第", page_index * 24 + production_index, "个视频资源，信息如下*****************"
+            # print "视频名称", production_box[1][0].text.lstrip()
+            # print "品牌名称：", production_box[2][0].text
+            # print "语种：", production_box[3][0].text
+            # print "国家或地区：", production_box[3][1].text
+            # print "广告年份：", production_box[4][0].text
+            # print "广告类型：", production_box[4][1].text
+            # video_cover_image_link = production_box[0][0][0].get('src')
+            # if video_cover_image_link[0:4] == "http":
+            #     print "广告视频封面图片链接:", production_box[0][0][0].get('src')
+            # else:
+            #     print "广告视频封面图片链接:", "http://k.cnad.com" + production_box[0][0][0].get('src')
+            # print "广告视频资源链接：", "http://k.cnad.com" + production_box[0][0].get('href')
+            # print "**************************分割线***************************************"
+            production_box_item = {
+                                    "视频名称" : production_box[1][0].text.lstrip(),
+                                    "品牌名称" : production_box[2][0].text,
+                                    "语种" : production_box[3][0].text,
+                                    "国家或地区" : production_box[3][1].text,
+                                    "广告年份" : production_box[4][0].text,
+                                    "广告类型" : production_box[4][1].text,
+                                    "广告视频封面图片链接" : "??",
+                                    "广告视频资源链接" : production_box[0][0].get('href')
+                                    }
+            # print production_box_item
+            # print json.dumps(production_box_item , ensure_ascii=False , indent=2)
+            production_box_item_list.append(production_box_item)
             production_index += 1
+
+    production_box_item_list_object = {
+                                        "所属分类" :category_item.get('title') ,
+                                        "视频资源集合" : production_box_item_list
+                                        }
+    production_box_item_list_total.append(production_box_item_list_object)
+
+# print production_box_item_list_total
+d = json.dumps(production_box_item_list_total , ensure_ascii=False , indent=2)
+with open("C:\\Users\\oliverfan\\Desktop\\111.txt", "w") as f:
+    json.dump(d, f)
+
+end_time = time.clock()
+time_duration = end_time - start_time
+
+print "Spider爬取工作完成。"
+print "共耗时："  + str(int(time_duration/60)) + " 分 " + str(int(math.ceil(time_duration%60))) + "秒"
